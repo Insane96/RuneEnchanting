0c98a39a5a2f6ee501bab8b9fe1a57f57619bca3
Insane96 <delvillano.alberto@gmail.com>
2026-04-29 17:23:09 +0200

    fixed blasting too strong

--- src/main/java/insane96mcp/runeenchanting/data/runes/BlastingRune.java
+++ src/main/java/insane96mcp/runeenchanting/data/runes/BlastingRune.java
@@ -1,21 +1,18 @@
 package insane96mcp.runeenchanting.data.runes;
 
 import insane96mcp.insanelib.core.feature.config.Config;
-import insane96mcp.runeenchanting.RuneEnchanting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.world.entity.EquipmentSlotGroup;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.DiggerItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.neoforge.event.ItemAttributeModifierEvent;
 import org.jetbrains.annotations.Nullable;
 
 public class BlastingRune extends Rune {
-    @Config(description = "Percentage bonus mining speed")
+    @Config(min = 0, description = "Percentage bonus mining speed")
     public static Double bonusMiningSpeed = 2.5d;
 
     @Override
@@ -35,13 +32,11 @@ public class BlastingRune extends Rune {
 
     @Override
     public float onMiningSpeed(Player player, ItemStack stack, BlockState state, @Nullable BlockPos pos, float original, float speed) {
-        if (state == null)
-            return 0f;
-        if (!stack.isCorrectToolForDrops(state))
-            return 0f;
-        if (!(stack.getItem() instanceof DiggerItem diggerItem))
-            return 0f;
+        if (state == null
+                || !stack.isCorrectToolForDrops(state)
+                || !(stack.getItem() instanceof DiggerItem diggerItem))
+            return super.onMiningSpeed(player, stack, state, pos, original, speed);
 
-        return speed + (Math.max(0.04f, (7f - state.getBlock().getExplosionResistance()) * bonusMiningSpeed.floatValue()) * diggerItem.getTier().getSpeed());
+        return speed + (Math.max(0.20f, (6f - state.getBlock().getExplosionResistance()) * bonusMiningSpeed.floatValue()) * diggerItem.getTier().getSpeed());
     }
 }
