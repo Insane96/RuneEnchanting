c09c1bca65035caa547c6a968b8a14afede8c64f
Insane96 <delvillano.alberto@gmail.com>
2026-05-17 09:20:22 +0200

    Added infos and config options to some curses

--- src/main/java/insane96mcp/runeenchanting/runes/curse/CurseOfBloodPact.java
+++ src/main/java/insane96mcp/runeenchanting/runes/curse/CurseOfBloodPact.java
@@ -1,10 +1,13 @@
 package insane96mcp.runeenchanting.runes.curse;
 
+import insane96mcp.insanelib.core.feature.config.Config;
 import insane96mcp.insanelib.event.HurtItemStackEvent;
 import insane96mcp.runeenchanting.RuneEnchanting;
 import insane96mcp.runeenchanting.runes.Rune;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.damagesource.DamageType;
@@ -15,6 +18,9 @@ import net.minecraft.world.item.ItemStack;
 public class CurseOfBloodPact extends Rune {
     public static final ResourceKey<DamageType> DAMAGE_TYPE = ResourceKey.create(Registries.DAMAGE_TYPE, RuneEnchanting.id("blood_pact"));
 
+    @Config(min = 0, max = 1)
+    public static Double chanceToHurt = 0.1d;
+
     @Override
     public String getName() {
         return "Curse of Blood Pact";
@@ -23,6 +29,11 @@ public class CurseOfBloodPact extends Rune {
     @Override
     public String getDescription() {
         return "Using the item can hurt the user";
+    }
+
+    @Override
+    public String getInfo() {
+        return "Chance to hurt: %s%%";
     }
 
     @Override
@@ -37,10 +48,15 @@ public class CurseOfBloodPact extends Rune {
 
         float damageAmount = 0f;
         for (int i = 0; i < event.getAmount(); i++) {
-            if (event.getRandom().nextInt(10) == 0)
+            if (event.getRandom().nextFloat() < chanceToHurt)
                 damageAmount++;
         }
         if (damageAmount > 0)
             player.hurt(player.damageSources().source(DAMAGE_TYPE), damageAmount);
+    }
+
+    @Override
+    public MutableComponent getInfoComponent() {
+        return Component.translatable(getInfoTranslationKey(), RuneEnchanting.NO_DECIMAL_FORMATTER.format(chanceToHurt * 100f));
     }
 }
--- src/main/java/insane96mcp/runeenchanting/runes/curse/CurseOfInefficiency.java
+++ src/main/java/insane96mcp/runeenchanting/runes/curse/CurseOfInefficiency.java
@@ -1,8 +1,12 @@
 package insane96mcp.runeenchanting.runes.curse;
 
+import insane96mcp.insanelib.core.feature.config.Config;
+import insane96mcp.runeenchanting.RuneEnchanting;
 import insane96mcp.runeenchanting.runes.Rune;
 import net.minecraft.core.BlockPos;
 import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -11,6 +15,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 public class CurseOfInefficiency extends Rune {
+
+    @Config(min = 0, max = 1)
+    public static Double miningSpeedReduction = 0.5d;
+
     @Override
     public String getName() {
         return "Curse of Inefficiency";
@@ -19,6 +27,11 @@ public class CurseOfInefficiency extends Rune {
     @Override
     public String getDescription() {
         return "Decreases mining speed";
+    }
+
+    @Override
+    public String getInfo() {
+        return "Mining speed penalty: %s%%";
     }
 
     @Override
@@ -35,5 +48,10 @@ public class CurseOfInefficiency extends Rune {
     @Override
     public float onMiningSpeed(Player player, ItemStack stack, BlockState state, @Nullable BlockPos pos, float original, float speed) {
         return speed * 0.5f;
+    }
+
+    @Override
+    public MutableComponent getInfoComponent() {
+        return Component.translatable(getInfoTranslationKey(), RuneEnchanting.NO_DECIMAL_FORMATTER.format(miningSpeedReduction * 100f));
     }
 }
